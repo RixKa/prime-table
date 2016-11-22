dd3fbd5212d59864931f3839a2e24c28f68e793d
Kayleigh Rix <kayleigh.rix@outplay.com>
2016-11-22 19:04:00 +0000

    [change] removing prime tests for bad values of n

--- code/test/test_primes.py
+++ code/test/test_primes.py
@@ -15,23 +15,11 @@ class TestIsPrime(object):
         assert is_prime(n)
 
     def test_n_not_prime(self):
-        n = random.choice([4, 6, 8, 10])
+        n = random.choice([0, 1, 4, 6, 8, 10])
         assert not is_prime(n)
-
-    def test_bad_value_n(self):
-        n = random.choice([ 'x', 'word', {'foo': 'bar'}, [0,1,2,3] ])
-
-        with pytest.raises(Exception) as e:
-            is_prime(n)
-        assert e.value.message
 
 
 class TestGetPrimes(object):
-    @pytest.mark.skip(reason="Not Implemented")
-    def test_n_equals_0(self):
-        n = 0
-        assert get_n_primes(n) == []
-
     def test_low_value_n(self):
         n = random.randint(1, 100)
         assert get_n_primes(n) == generate_n_primes(n)
@@ -43,16 +31,3 @@ class TestGetPrimes(object):
     def test_higher_value_n(self):
         n = random.randint(10000, 15000)
         assert get_n_primes(n) == generate_n_primes(n)
-
-    @pytest.mark.skip(reason="Not Implemented")
-    def test_n_is_digit(self):
-        n = random.choice([ '1', '3', '5', '7' ])
-        assert get_n_primes(n) == generate_n_primes(int(n))
-
-    @pytest.mark.skip(reason="Not Implemented")
-    def test_bad_value_n(self):
-        n = random.choice([ 'x', 'word', {'foo': 'bar'}, [0,1,2,3] ])
-
-        with pytest.raises(Exception) as e:
-            get_n_primes(n)
-        assert e.value.message
